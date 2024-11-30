75cb24b16c03a532d04b0e58d66e556910b40c1e
Vivek Yadav <enpvivek@gmail.com>
2024-11-30 19:22:40 +0530

    change acc

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      {" "}
-      hi
+      hello world
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
